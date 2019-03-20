f2db72cb26ed9763098a9cad8184de1abe2538f2
Misha Tsyvis <mtsyvis@outlook.com>
2019-03-20 19:38:20 +0300

    implement quick sort without xml commens

---
+++ NET1.S.2019.Tsyvis.01/NET1.S.2019.Tsyvis.01/ArrayExtension.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET1.S._2019.Tsyvis._01
+{
+    /// <summary>
+    /// Class ArrayExtension
+    /// </summary>
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// Sorts the elements in one-dimensional integer array using a quicksort algorithm.
+        /// </summary>
+        /// <param name="array">The one-dimensional integer array to sort.</param>
+        /// <param name="start">The starting index of the range to sort.</param>
+        /// <param name="length">The number of elements in the range to sort.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void QuickSort(int[] array, int start, int length)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (start < 0 || length < 0 || start + length > array.Length)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            QuickSortImplementation(array, start, start + length - 1);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        public static void QuickSort(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            QuickSortImplementation(array, 0, array.Length - 1);
+        }
+
+        private static void QuickSortImplementation(int[] array, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            int pivot = Partition(array, start, end);
+            QuickSortImplementation(array, start, pivot - 1);
+            QuickSortImplementation(array, pivot + 1, end);
+        }
+
+        private static int Partition(int[] array, int start, int end)
+        {
+            int temp;
+            int marker = start;
+
+            for (int i = start; i <= end; i++)
+            {
+                if (array[i] < array[end])
+                {
+                    temp = array[marker];
+                    array[marker] = array[i];
+                    array[i] = temp;
+                    marker += 1;
+                }
+            }
+
+            temp = array[marker];
+            array[marker] = array[end];
+            array[end] = temp;
+            return marker;
+        }
+
+        //public static void MergeSort(int[] input, int low, int high)
+        //{
+        //    if (low < high)
+        //    {
+        //        int middle = (low / 2) + (high / 2);
+        //        MergeSort(input, low, middle);
+        //        MergeSort(input, middle + 1, high);
+        //        Merge(input, low, middle, high);
+        //    }
+        //}
+
+        //public static void MergeSort(int[] input)
+        //{
+        //    MergeSort(input, 0, input.Length - 1);
+        //}
+
+        //private static void Merge(int[] input, int low, int middle, int high)
+        //{
+
+        //    int left = low;
+        //    int right = middle + 1;
+        //    int[] tmp = new int[(high - low) + 1];
+        //    int tmpIndex = 0;
+
+        //    while ((left <= middle) && (right <= high))
+        //    {
+        //        if (input[left] < input[right])
+        //        {
+        //            tmp[tmpIndex] = input[left];
+        //            left = left + 1;
+        //        }
+        //        else
+        //        {
+        //            tmp[tmpIndex] = input[right];
+        //            right = right + 1;
+        //        }
+        //        tmpIndex = tmpIndex + 1;
+        //    }
+
+        //    if (left <= middle)
+        //    {
+        //        while (left <= middle)
+        //        {
+        //            tmp[tmpIndex] = input[left];
+        //            left = left + 1;
+        //            tmpIndex = tmpIndex + 1;
+        //        }
+        //    }
+
+        //    if (right <= high)
+        //    {
+        //        while (right <= high)
+        //        {
+        //            tmp[tmpIndex] = input[right];
+        //            right = right + 1;
+        //            tmpIndex = tmpIndex + 1;
+        //        }
+        //    }
+
+        //    for (int i = 0; i < tmp.Length; i++)
+        //    {
+        //        input[low + i] = tmp[i];
+        //    }
+
+        //}
+
+    }
+}
---
+++ NET1.S.2019.Tsyvis.01/NUnit.Test.Project/ArrayExtensionNUnitTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NET1.S._2019.Tsyvis._01;
+
+namespace NUnit.Test.Project
+{
+    [TestFixture]
+    public class ArrayExtensionNUnitTests
+    {
+        private const int BigArraySize = 10000;
+
+        [Test]
+        public void QuickSort_RandomValueArray_WellSortedArray()
+        {   
+            // Arrange
+            Random random = new Random();
+
+            int[] expectedArray = new int[BigArraySize];
+            int[] actualArray = new int[BigArraySize];
+
+            for (int i = 0; i < expectedArray.Length; i++)
+            {
+                expectedArray[i] = random.Next();
+            }
+            
+            Array.Copy(expectedArray, actualArray, expectedArray.Length);
+
+            // Act
+            ArrayExtension.QuickSort(expectedArray);
+            Array.Sort(actualArray);
+           
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, actualArray);
+        }
+
+        [Test]
+        public void QuickSort_RandomValueArray_WellSortedArrayInRange()
+        {
+            // Arrange
+            Random random = new Random();
+
+            int[] expectedArray = new int[BigArraySize];
+            int[] actualArray = new int[BigArraySize];
+
+            for (int i = 0; i < expectedArray.Length; i++)
+            {
+                expectedArray[i] = random.Next();
+            }
+
+            Array.Copy(expectedArray, actualArray, expectedArray.Length);
+
+            // Act
+            ArrayExtension.QuickSort(expectedArray, BigArraySize/3, BigArraySize/2);
+            Array.Sort(actualArray, BigArraySize / 3, BigArraySize / 2);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedArray, actualArray);
+        }
+    }
+}
