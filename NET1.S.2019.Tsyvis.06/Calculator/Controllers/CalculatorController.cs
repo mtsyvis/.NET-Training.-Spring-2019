26e5cc6c30016a2f966dd92bf3d1b2e404761807
Misha Tsyvis <mtsyvis@outlook.com>
2019-04-05 18:12:30 +0300

    Add ASP.NET MVC project

---
+++ NET1.S.2019.Tsyvis.06/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using NET1.S._2019.Tsyvis._06;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CalculateGcd(int number1, int number2, string algorithm)
+        {
+            ViewBag.Number1 = number1;
+            ViewBag.Number2 = number2;
+            int gcd = 0;
+            long millisecond = 0;
+            switch (algorithm)
+            {
+                case "binary":
+                    gcd = GCDAlgorithms.CalculateGcdBySteinAndTime(number1, number2, out millisecond);
+                    ViewBag.Algorithms = "Stain algorithm";
+                    break;
+
+                case "euclidean":
+                    gcd = GCDAlgorithms.CalculateGcdByEuclideanAndTime(number1, number2, out millisecond);
+                    ViewBag.Algorithms = "Euclidean algorithm";
+                    break;
+            }
+
+            ViewBag.Millisecond = millisecond;
+            ViewBag.GCD = gcd;
+
+            return View();
+        }
+    }
+}
--- NET1.S.2019.Tsyvis.06/GCD.Tests/GCDAlgorithmsTests.cs
+++ NET1.S.2019.Tsyvis.06/GCD.Tests/GCDAlgorithmsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using NET1.S._2019.Tsyvis._06;
 
 namespace GCD.Tests
