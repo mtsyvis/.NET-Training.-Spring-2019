8362b901989bb12e6cabb1cae2043361a2b4ab72
Misha Tsyvis <mtsyvis@outlook.com>
2019-04-13 18:31:52 +0300

    Done Binary Search in day11

--- NET1.S.2019.Tsyvis.11/NET1.S.2019.Tsyvis.11.Tests/ArrayExtensionsTests.cs
+++ NET1.S.2019.Tsyvis.11/NET1.S.2019.Tsyvis.11.Tests/ArrayExtensionsTests.cs
@@ -10,6 +10,9 @@ namespace NET1.S._2019.Tsyvis._11.Tests
         [TestCase(new int[] { 2, 6, 10, 12, 30 }, 10, ExpectedResult = 2)]
         [TestCase(new int[] { 2, 6, 10, 12, 30 }, -999923, ExpectedResult = -1)]
         [TestCase(new int[] { 100, 50, 25, 15, 10, -999 }, 10, ExpectedResult = 4)]
+        [TestCase(new int[] { }, 7, ExpectedResult = -1)]
+        [TestCase(new int[] { 1 }, 1, ExpectedResult = 0)]
+        [TestCase(new int[] { 1, 2 }, 2, ExpectedResult = 1)]
         public int BinarySearch_IntArray(int[] array, int value) => array.BinarySearch(value);
 
         [TestCase(new string[] { "AAA", "aaa", "bbb", "fff" }, "bbb", ExpectedResult = 2)]
