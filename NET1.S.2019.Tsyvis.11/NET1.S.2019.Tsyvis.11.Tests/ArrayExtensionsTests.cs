0ebdf6901b58461997e672c004f7214c0ecf9f60
Misha Tsyvis <mtsyvis@outlook.com>
2019-04-13 18:25:52 +0300

    Done Binary Search in day11

---
+++ NET1.S.2019.Tsyvis.11/NET1.S.2019.Tsyvis.11.Tests/ArrayExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace NET1.S._2019.Tsyvis._11.Tests
+{
+    [TestFixture]
+    public class ArrayExtensionsTests
+    {
+        [TestCase(new int[] { -6, 0, 2, 10, 22, 100 }, -6, ExpectedResult = 0)]
+        [TestCase(new int[] { -6, 0, 2, 10, 22, 100 }, 100, ExpectedResult = 5)]
+        [TestCase(new int[] { 2, 6, 10, 12, 30 }, 10, ExpectedResult = 2)]
+        [TestCase(new int[] { 2, 6, 10, 12, 30 }, -999923, ExpectedResult = -1)]
+        [TestCase(new int[] { 100, 50, 25, 15, 10, -999 }, 10, ExpectedResult = 4)]
+        public int BinarySearch_IntArray(int[] array, int value) => array.BinarySearch(value);
+
+        [TestCase(new string[] { "AAA", "aaa", "bbb", "fff" }, "bbb", ExpectedResult = 2)]
+        [TestCase(new string[] { "yyy", "fff", "aaa", "YYY" }, "YYY", ExpectedResult = 3)]
+        [TestCase(new string[] { "ASD", "sdfsdf", "AASD" }, "bbb", ExpectedResult = -1)]
+        public int BinarySearch_StringArray(string[] array, string value) => array.BinarySearch(value);
+    }
+}
---
+++ NET1.S.2019.Tsyvis.11/NET1.S.2019.Tsyvis.11/ArrayExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace NET1.S._2019.Tsyvis._11
+{
+    /// <summary>
+    /// Provides a binary search for elements in an array
+    /// </summary>
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Binaries the search.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the array.
+        /// </typeparam>
+        /// <param name="array">The array.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        /// The index of the specified value in the specified array,
+        /// if value is found; otherwise, a negative number.
+        /// </returns>
+        public static int BinarySearch<T>(this T[] array, T value)
+            where T : IComparable<T>
+        {
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+
+            bool descendingOrder = array[0].CompareTo(array[array.Length - 1]) > 0;
+
+            return BinarySearchImplementation(array, descendingOrder, 0, array.Length, value);
+        }
+
+        private static int BinarySearchImplementation<T>(T[] array, bool descendingOrder, int left, int right, T value)
+            where T : IComparable<T>
+        {
+            int middle = left + (right - left) / 2;
+
+            if (left >= right)
+            {
+                return -1;
+            }
+
+            if (array[middle].Equals(value))
+            {
+                return middle;
+            }
+
+            if ((array[middle].CompareTo(value) > 0) ^ descendingOrder)
+            {
+                return BinarySearchImplementation<T>(array, descendingOrder, left, middle, value);
+            }
+            else
+            {
+                return BinarySearchImplementation<T>(array, descendingOrder, middle + 1, right, value);
+            }
+        }
+    }
+}
