ce424ea24c085eae6043bd9ae5b53044e0659cc1
Misha Tsyvis <mtsyvis@outlook.com>
2019-04-17 19:43:40 +0300

    Done task2 in day12

---
+++ NET1.S.2019.Tsyvis.12/NET1.S.2019.Tsyvis.12.Tests/BracketsVerifierTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NET1.S._2019.Tsyvis._12.Tests
+{
+    [TestFixture]
+    public class BracketsVerifierTests
+    {
+        [TestCase("([])", ExpectedResult = true)]
+        [TestCase("( ([])   { [{ }] } )", ExpectedResult = true)]
+        [TestCase("( [ )", ExpectedResult = false)]
+        [TestCase("]]]]]]", ExpectedResult = false)]
+        [TestCase("vasya", ExpectedResult = false)]
+        [TestCase("}}}}}}(", ExpectedResult = false)]
+        public bool AreBracketsCorrectlySpacedTest(string str) => BracketsVerifier.AreBracketsCorrectlySpaced(str);
+
+        [Test]
+        public void AreBracketsCorrectlySpaced_StringIsNullThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => BracketsVerifier.AreBracketsCorrectlySpaced(null));
+        }
+        
+
+    }
+}
---
+++ NET1.S.2019.Tsyvis.12/NET1.S.2019.Tsyvis.12/BracketsVerifier.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NET1.S._2019.Tsyvis._12
+{
+    /// <summary>
+    /// Provide verification in string.
+    /// </summary>
+    public static class BracketsVerifier
+    {
+        /// <summary>
+        /// Checks the string for the matching brackets.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns>True if string is matched;otherwise - false. And false if string don't have brackets.</returns>
+        /// <exception cref="ArgumentNullException">string is null</exception>
+        public static bool AreBracketsCorrectlySpaced(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException($"string is null {nameof(str)}");
+            }
+
+            var stack = new Stack<char>();
+
+            foreach (var symbol in str)
+            {
+                if (symbol == '(' || symbol == '[' || symbol == '{')
+                {
+                    stack.Push(symbol);
+                    continue;
+                }
+
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                char top;
+                switch (symbol)
+                {
+                    case ')':
+                        top = stack.Peek();
+                        stack.Pop();
+                        if (top == '{' || top == '[')
+                        {
+                            return false;
+                        }
+
+                        break;
+
+                    case ']':
+                        top = stack.Peek();
+                        stack.Pop();
+                        if (top == '{' || top == '(')
+                        {
+                            return false;
+                        }
+
+                        break;
+
+                    case '}':
+                        top = stack.Peek();
+                        stack.Pop();
+                        if (top == '[' || top == '(')
+                        {
+                            return false;
+                        }
+
+                        break;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
