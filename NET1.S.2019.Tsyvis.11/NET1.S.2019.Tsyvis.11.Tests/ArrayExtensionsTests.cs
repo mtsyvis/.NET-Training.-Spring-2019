6488436f7c336b7abcb6d459eb8675d2ff6f8ad2
mtsyvis <mtsyvis2405@gmail.com>
2019-04-21 10:12:42 +0300

    Corrected Binary Search in day11

--- NET1.S.2019.Tsyvis.11/NET1.S.2019.Tsyvis.11.Tests/ArrayExtensionsTests.cs
+++ NET1.S.2019.Tsyvis.11/NET1.S.2019.Tsyvis.11.Tests/ArrayExtensionsTests.cs
@@ -1,23 +1,58 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace NET1.S._2019.Tsyvis._11.Tests
 {
     [TestFixture]
     public class ArrayExtensionsTests
     {
-        [TestCase(new int[] { -6, 0, 2, 10, 22, 100 }, -6, ExpectedResult = 0)]
+        [TestCase(new int[] { -6, 0, 2, 10, 22, 100 }, 22, ExpectedResult = 4)]
         [TestCase(new int[] { -6, 0, 2, 10, 22, 100 }, 100, ExpectedResult = 5)]
-        [TestCase(new int[] { 2, 6, 10, 12, 30 }, 10, ExpectedResult = 2)]
+        [TestCase(new int[] { 2, 6, 10, 12, 30 }, 2, ExpectedResult = 0)]
         [TestCase(new int[] { 2, 6, 10, 12, 30 }, -999923, ExpectedResult = -1)]
-        [TestCase(new int[] { 100, 50, 25, 15, 10, -999 }, 10, ExpectedResult = 4)]
         [TestCase(new int[] { }, 7, ExpectedResult = -1)]
         [TestCase(new int[] { 1 }, 1, ExpectedResult = 0)]
         [TestCase(new int[] { 1, 2 }, 2, ExpectedResult = 1)]
-        public int BinarySearch_IntArray(int[] array, int value) => array.BinarySearch(value);
+        public int BinarySearch_IntArray(int[] array, int value) => ArrayExtensions.BinarySearch(array, value, null);
 
         [TestCase(new string[] { "AAA", "aaa", "bbb", "fff" }, "bbb", ExpectedResult = 2)]
         [TestCase(new string[] { "yyy", "fff", "aaa", "YYY" }, "YYY", ExpectedResult = 3)]
         [TestCase(new string[] { "ASD", "sdfsdf", "AASD" }, "bbb", ExpectedResult = -1)]
         public int BinarySearch_StringArray(string[] array, string value) => array.BinarySearch(value);
+
+        [TestCase(new int[] { -6, 22, 0, 2, 10, 22, 100 }, 2, 3, 22, ExpectedResult = 5)]
+        [TestCase(new int[] { 2, 6, 10, 12, 30, 2 }, 0, 3, 2, ExpectedResult = 0)]
+        [TestCase(new int[] { 2, 6, 10, 12, 30, 30 }, 2, 3, 30, ExpectedResult = 4)]
+        public int BinarySearch_IntArrayWithSetIndexAndLength(int[] array, int index, int length, int value) => array.BinarySearch(index, length, value);
+
+        [Test]
+        public void BinarySearch_InvalidIndexOrLength_ThrowArgumentException()
+        {
+            var array = new int[] { 1, 4 };
+            try
+            {
+                array.BinarySearch<int>(0, 10, 1);
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+                Assert.IsNotNull(e);
+            }
+        }
+
+        [Test]
+        public void BinarySearch_IndexOrLengthLessThenZero_ThrowArgumentException()
+        {
+            var array = new int[] { 1, 4 };
+            try
+            {
+                array.BinarySearch<int>(-3, -3, 1, null);
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+                Assert.IsNotNull(e);
+            }
+        }
     }
 }
--- NET1.S.2019.Tsyvis.11/NET1.S.2019.Tsyvis.11/ArrayExtensions.cs
+++ NET1.S.2019.Tsyvis.11/NET1.S.2019.Tsyvis.11/ArrayExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NET1.S._2019.Tsyvis._11
 {
@@ -26,12 +27,108 @@ namespace NET1.S._2019.Tsyvis._11
                 return -1;
             }
 
-            bool descendingOrder = array[0].CompareTo(array[array.Length - 1]) > 0;
-
-            return BinarySearchImplementation(array, descendingOrder, 0, array.Length, value);
+            return BinarySearchImplementation(array, 0, array.Length, value, Comparer<T>.Default);
         }
 
-        private static int BinarySearchImplementation<T>(T[] array, bool descendingOrder, int left, int right, T value)
+        /// <summary>
+        /// Binaries the search.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the array</typeparam>
+        /// <param name="array">The array.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="comparer">The comparer.</param>
+        /// <returns>The index of the specified value in the specified array,
+        /// if value is found; otherwise, a negative number.
+        /// </returns>
+        public static int BinarySearch<T>(this T[] array, T value, IComparer<T> comparer)
+            where T : IComparable<T>
+        {
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+
+            if (comparer == null)
+            {
+                comparer = Comparer<T>.Default;
+            }
+
+            return BinarySearchImplementation(array, 0, array.Length, value, comparer);
+        }
+
+        /// <summary>
+        /// Binaries the search.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the array</typeparam>
+        /// <param name="array">The array.</param>
+        /// <param name="index">The index.</param>
+        /// <param name="length">The length.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>The index of the specified value in the specified array,
+        /// if value is found; otherwise, a negative number.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">index or length less are negative</exception>
+        /// <exception cref="ArgumentException">argument invalid</exception>
+        public static int BinarySearch<T>(this T[] array, int index, int length, T value)
+            where T : IComparable<T>
+        {
+            if (index < 0 || length < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(index)} < 0 or {nameof(length)} < 0");
+            }
+
+            if (array.Length - index < length)
+            {
+                throw new ArgumentException($"argument invalid");
+            }
+
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+
+            return BinarySearchImplementation(array, index, index + length + 1, value, Comparer<T>.Default);
+        }
+
+        /// <summary>
+        /// Binaries the search.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of the array</typeparam>
+        /// <param name="array">The array.</param>
+        /// <param name="index">The index.</param>
+        /// <param name="length">The length.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="comparer">The comparer.</param>
+        /// <returns>The index of the specified value in the specified array,
+        /// if value is found; otherwise, a negative number.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">index or length less are negative</exception>
+        /// <exception cref="ArgumentException">argument invalid</exception>
+        public static int BinarySearch<T>(this T[] array, int index, int length, T value, IComparer<T> comparer)
+            where T : IComparable<T>
+        {
+            if (index < 0 || length < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(index)} < 0 or {nameof(length)} < 0");
+            }
+
+            if (array.Length - index < length)
+            {
+                throw new ArgumentException($"argument invalid");
+            }
+
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+
+            if (comparer == null)
+            {
+                comparer = Comparer<T>.Default;
+            }
+
+            return BinarySearchImplementation(array, index, index + length + 1, value, comparer);
+        }
+
+        private static int BinarySearchImplementation<T>(T[] array, int left, int right, T value, IComparer<T> comparer)
             where T : IComparable<T>
         {
             int middle = left + (right - left) / 2;
@@ -41,18 +138,18 @@ namespace NET1.S._2019.Tsyvis._11
                 return -1;
             }
 
-            if (array[middle].Equals(value))
+            if (comparer.Compare(array[middle], value) == 0)
             {
                 return middle;
             }
 
-            if ((array[middle].CompareTo(value) > 0) ^ descendingOrder)
+            if (comparer.Compare(array[middle], value) > 0)
             {
-                return BinarySearchImplementation<T>(array, descendingOrder, left, middle, value);
+                return BinarySearchImplementation<T>(array, left, middle, value, comparer);
             }
             else
             {
-                return BinarySearchImplementation<T>(array, descendingOrder, middle + 1, right, value);
+                return BinarySearchImplementation<T>(array, middle + 1, right, value, comparer);
             }
         }
     }
