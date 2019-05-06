85ca4e76fdf478fbbaba0a8a653a72c10fd0e05a
mtsyvis <mtsyvis2405@gmail.com>
2019-05-06 17:47:28 +0300

    Done day19

---
+++ NET1.S.2019.Tsyvis.19/NET1.S.2019.Tsyvis.19.Tests/BookFormatProviderTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET1.S._2019.Tsyvis._19.Tests
+{
+    public class BookFormatProviderTest : IFormatProvider, ICustomFormatter
+    {
+        public object GetFormat(Type formatType)
+        {
+            return formatType == typeof(ICustomFormatter) ? this : null;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            var book = (Book)arg;
+            return $"{book.Pages}, {book.Price}";
+        }
+    }
+}
---
+++ NET1.S.2019.Tsyvis.19/NET1.S.2019.Tsyvis.19.Tests/BookTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NET1.S._2019.Tsyvis._19.Tests
+{
+    [TestFixture]
+    public class BookTests
+    {
+        private Book book = new Book
+                                {
+                                    Title = "C# in Depth",
+                                    Author = "Jon Skeet",
+                                    Year = 2019,
+                                    PublishingHouse = "Manning",
+                                    Edition = 4,
+                                    Pages = 900,
+                                    Price = 40
+                                };
+
+        [TestCase("ATYPH", ExpectedResult = "Jon Skeet, C# in Depth, 2019, \"Manning\"")]
+        [TestCase("ATY", ExpectedResult = "Jon Skeet, C# in Depth, 2019")]
+        [TestCase("AT", ExpectedResult = "Jon Skeet, C# in Depth")]
+        [TestCase("TPH", ExpectedResult = "C# in Depth, \"Manning\"")]
+        [TestCase("TYPH", ExpectedResult = "C# in Depth, 2019, \"Manning\"")]
+        [TestCase("T", ExpectedResult = "C# in Depth")]
+        public string ToStringTest_Format(string format) => this.book.ToString(format);
+
+        [Test]
+        public void ToStringTest_InvalidFormat_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => this.book.ToString("BLABLA"));
+        }
+
+        [Test]
+        public void ToStringTest_FormatProviderTest()
+        {
+            Assert.AreEqual("900, 40", this.book.ToString(new BookFormatProviderTest()));
+        }
+    }
+}
